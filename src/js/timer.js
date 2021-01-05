7c7abed4124e91df4e4e99e90f3689b3e46ccb49
IvettaDashkova <ivettazakonno@gmail.com>
2021-01-06 01:25:01 +0200

    добавляет класс в таймер

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,38 +1,67 @@
-const daysText = document.querySelector('[data-value="days"]');
-const hoursText = document.querySelector('[data-value="hours"]');
-const minsText = document.querySelector('[data-value="mins"]');
-const secsText = document.querySelector('[data-value="secs"]');
 
+class CountdownTimer {
+  constructor({ selector, targetDate, name }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.name = name;
+    this.currentDate;
+    this.deltaTime;
+    this.currentDate;
+  }
+  insertName() {
+    const name = document.querySelector('#insert-name');
+    name.textContent = `До Дня Рождения ${this.name} осталось:`;
+    this.getIdDate();
+  }
+  getIdDate() {
+    this.targetDate = new Date(this.targetDate).getTime();
+    this.getTimer();
+  }
+  getTimer() {
+    setInterval(() => {
+      this.currentDate = Date.now();
+      this.deltaTime = this.targetDate - this.currentDate;
+      this.timing(this.deltaTime);
+    }, 1000);
+  }
+  timing() {
+    const daysText = document.querySelector('[data-value="days"]');
+    const hoursText = document.querySelector('[data-value="hours"]');
+    const minsText = document.querySelector('[data-value="mins"]');
+    const secsText = document.querySelector('[data-value="secs"]');
 
-function timer() {
-  //Mon Aug 09 2021 00:00:00
-  const futureDate = new Date(1628456400000);
-  setInterval(() => {
-    const currentDate = Date.now();
-    const deltaTime = futureDate - currentDate;
-    timing(deltaTime);
-  }, 1000);
-}
-function timing(time) {
- 
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-daysText.textContent = `${days}`
-  
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-hoursText.textContent = `${hours}`;
- 
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-minsText.textContent = `${mins}`;
- 
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    const days = Math.floor(this.deltaTime / (1000 * 60 * 60 * 24));
+    daysText.textContent = `${days}`;
+
+    const hours = this.pad(
+      Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    hoursText.textContent = `${hours}`;
+
+    const mins = this.pad(
+      Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+    );
+    minsText.textContent = `${mins}`;
+
+    const secs = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
     secsText.textContent = `${secs}`;
-    
-}
-function pad(value) {
-    return String(value).padStart(2, '0')
+  }
+
+  pad(time) {
+    return String(time).padStart(2, '0');
+  }
 }
 
+const hbIvetta = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Aug 09, 2021'),
+  name: 'Ivetta'
+});
+//hbIvetta.insertName();
 
-timer()
+const hbEd = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 24, 2021'),
+  name: 'Ed'
+})
+hbEd.insertName()
